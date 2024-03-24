4265ae3fb8f4be43d957150d7fb815a9d745ba1d
Yash Raj Gupta <yashraj.guptadev@gmail.com>
2024-03-24 19:31:18 +0530

    frontend modified

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UploadPage from "./components/UploadPage";
 
+
 function App() {
   return(
 <>
--- client/src/components/UploadPage.jsx
+++ client/src/components/UploadPage.jsx
@@ -4,16 +4,18 @@ function UploadPage() {
 
     const [selectedFile, setSelectedFile] = useState(null);
     const [imageUrl, setImageUrl] = useState(null);
+    const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
     const handleFileChange = (event) => {
         setSelectedFile(event.target.files[0]);
-        setImageUrl(null); // Reset imageUrl when a new file is selected
+        setImageUrl(null); 
+        setIsButtonDisabled(false);
       };
     
       const handleUpload = async () => {
         const formData = new FormData();
         formData.append('image', selectedFile);
-    
+        setIsButtonDisabled(true);
         try {
           const response = await axios.post('http://localhost:5000/upload', formData, {
             headers: {
@@ -21,7 +23,7 @@ function UploadPage() {
             },
           });
           setImageUrl(`data:image/jpeg;base64,${response.data.image}`);
-          console.log(response);
+          
         } catch (error) {
           console.error('Error uploading image: ', error);
         }
@@ -31,35 +33,49 @@ function UploadPage() {
     <div className="bg-gray-100 min-h-screen">
     <nav className="bg-gray-800 text-white p-4">
       <div className="container mx-auto flex items-center justify-center">
-        <h1 className="text-2xl font-bold">Image Upload App</h1>
+        <h1 className="text-2xl font-bold">Vehicle Detection app </h1>
       </div>
     </nav>
-    <div className="container mx-auto mt-10 flex flex-col justify-center items-center">
+    <div className="mx-auto border-black flex flex-row justify-center items-center">
+    <div className="shadow-2xl mt-10 min-w-[50%] rounded-xl flex flex-col justify-center items-center bg-gray-300 p-4">
       <h2 className="text-2xl font-bold mb-4">Upload Image</h2>
-      <input type="file" onChange={handleFileChange} />
+      <input type="file"
+      accept='image/*'
+       onChange={handleFileChange} className='text-lg font-medium border-2 border-gray-500 rounded-lg p-2 hover:bg-gray-200 focus:outline-none focus:border-gray-700'/>
  
-      {selectedFile && (
+      {(selectedFile && !imageUrl) && (
           <div className="mt-8 flex flex-col justify-center items-center">
             <h2 className="text-2xl font-bold mb-4">Selected Image</h2>
             <div className='w-[100%] h-[75%] items-center'>
-            <img src={URL.createObjectURL(selectedFile)} alt="Selected" className="w-1/2 mx-auto h-[50%]" />
+            <img src={URL.createObjectURL(selectedFile)} alt="Selected" className="w-1/2 mx-auto h-[50%] transform transition-transform hover:scale-110" />
             </div>
          
           </div>
         )}
-             <button className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 ${!selectedFile ? 'opacity-50 cursor-not-allowed' : ''}`} 
+             <button className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 ${isButtonDisabled ? 'opacity-50 cursor-not-allowed' : ''}`} 
               onClick={handleUpload}
-               disabled={!selectedFile} >
+               disabled={isButtonDisabled} >
         Upload
       </button>
-      {imageUrl && (
+      {imageUrl && (<div className='flex flex-row justify-center items-center '>
         <div className="mt-8">
-          <h2 className="text-2xl font-bold mb-4">Processed Image</h2>
-      
-          <img src={imageUrl} alt="Processed" className="max-w-lg" />
+          <h2 className="text-2xl font-bold mb-4 items-center flex justify-center">Uploaded Image</h2>
+          <div className='w-[100%] h-[100%] items-center'>
+            <img src={URL.createObjectURL(selectedFile)} alt="Selected" className="w-[75%] mx-auto h-[75%] transform transition-transform hover:scale-110" />
+            </div>
         </div>
+         <div className="mt-8">
+         <h2 className="text-2xl font-bold mb-4 justify-center flex">Processed Image</h2>
+         <div className='w-[100%] h-[100%] items-center'>
+            <img src={imageUrl} alt="Processed" className="w-[75%] mx-auto h-[75%] transform transition-transform hover:scale-110" />
+            </div>
+       </div>
+      </div>
+        
       )}
     </div>
+    </div>
+    
   </div>
   )
 }
